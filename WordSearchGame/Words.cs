b1b63fd7448032412497b5c4f8729f56c0ec8bd5
Alexandre Tavares <prodalexandretavares@gmail.com>
2021-05-10 23:08:34 +0100

    Classes created

---
+++ WordSearchGame/Words.cs
@@ -0,0 +1,34 @@
+﻿/*
+    +-------------------------------------------------------+
+    |                       WORDS                           |
+    +-------------------------------------------------------+
+    |                                                       |
+    |   - playID            :  int                          |
+    |   - numChar           :  int                          |
+    |                                                       |
+    +-------------------------------------------------------+
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordSearchGame
+{
+    class Words
+    {
+        private int playID;  //Play where the word was used 
+        private int numChar; //Number of caracters of the word
+
+        public Words(int playID, int numChar)
+        {
+            this.playID = playID; //Stores in waht play the word is used
+            this.numChar = numChar; //Number of caracters of the word
+        }
+
+        public int PlayID { get => playID; set => playID = value; }
+        public int NumChar { get => numChar; set => numChar = value; }
+    }
+}
